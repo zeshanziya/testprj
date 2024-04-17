c57876fc5e045157dd0b97989d121c3b3e0d8a65
zeshanziya <zeshan.ziya@axelerant.com>
2024-04-17 17:19:19 +0530

    Initial Version

---
+++ src/Test.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Zeshanziya\Testprj;
+
+/**
+ * A test class.
+ */
+class Test {
+
+  /**
+   * A test method.
+   *
+   * @return string
+   *   The test result.
+   */
+  public function dump() {
+    return 'Hello World!';
+  }
+
+}
